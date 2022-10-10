6ebc8fcdbe3f485d52c39c45bba526ea3ca9a11f
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-10-10 14:27:11 -0300

    Atualizando dados API
    npm install
    npm run api

--- src/servicos/apiLeiloes.js
+++ src/servicos/apiLeiloes.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // create axios
 const instance = axios.create({
-  baseURL: 'http://192.168.1.3:3000/',
+  baseURL: 'http://172.26.22.146:3000/',
   timeout: 1000,
 });
 
